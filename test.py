a1ef6f2da22f175092da6a18c8567f0a35097cfc
stevenvorona <stevenvorona@gmail.com>
2017-09-10 02:40:48 +0000

    atest

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
+import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
+time.sleep(5)
 for i in range(18, 26):
     GPIO.setup(i, GPIO.OUT, initial=GPIO.LOW)
 
 for i in range(0, 8):
     GPIO.output(i+18, 1)
-    time.sleep(0.3)
+    time.sleep(2)
     GPIO.output(i+18, 0)
-    time.sleep(0.3)
+    
