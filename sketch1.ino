fd54da05106bf834b6120d1d2283fa46970f37e9
Sammy Lyle <sammylyle@Sammys-MacBook-Pro.local>
2018-05-05 10:12:08 -0400

    f

--- sketch1.ino
+++ sketch1.ino
@@ -1,1 +1,26 @@
 //sketch
+String in = "";
+
+byte[] hexToByte(String coords){
+  String newcoords = coords;
+  byte[] bytes  = new byte[coords.length()/2]
+  for(int i = 0; i < bytes.length; i++){
+    bytes[i] = newcoords.substring(0, 2);
+    newcoords = newcoords.substring(0, 2);
+  }
+}
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(a,OUTPUT);
+  //...
+}
+
+void loop(){
+  if(Serial.available()){
+    //read bytes
+  }
+  else{
+    //display previous char
+  }
+}
