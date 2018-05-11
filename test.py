2d9164002ceaea323dcb8b43f3b6dd0eb86a48f4
stevenvorona <stevenvorona@gmail.com>
2018-05-11 12:25:00 -0400

    fixed test

--- test.py
+++ test.py
@@ -3,8 +3,17 @@ GPIO.setmode(GPIO.BCM)
 for i in range(18, 26):
     GPIO.setup(i, GPIO.OUT, initial=GPIO.LOW)
 
+for i in range(0, 128):
+    bitstr=str('0.08b'.format(i))
+    for i in range(0, 8):
+        GPIO.output(i+18, int(bitstr[i]))
+    #waits 1 second before moving to the next LED
+    time.sleep(1)
+    for i in range(0, 8):
+        GPIO.output(i+18, 0)
+'''
 for i in range(0, 8):
     GPIO.output(i+18, 1)
-    time.sleep(0.3)
+    time.sleep(2)
     GPIO.output(i+18, 0)
-    time.sleep(0.3)
+'''
